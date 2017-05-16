a9e4d04714eaf9df558d0faffd4261132b08d2f6
Wudi Wang <wwoody827@gmail.com>
2017-05-15 22:15:19 -0400

    nvidia model

--- model.py
+++ model.py
@@ -1,44 +1,48 @@
 import cv2
 import csv
 import numpy as np
+import random, math
+import sklearn
 
-# read data
-lines = []
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        random.shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+                name = './data/IMG/'+batch_sample[0].split('/')[-1]
+                center_image = cv2.imread(name)
+                center_angle = float(batch_sample[3])
+                images.append(center_image)
+                angles.append(center_angle)
+
+            # trim image to only see section with road
+            X_train = np.array(images)
+            y_train = np.array(angles)
+            yield sklearn.utils.shuffle(X_train, y_train)
+            
+samples = []
 csv_filename = './data/driving_log.csv'
 with open(csv_filename, 'r') as csv_file:
     reader = csv.reader(csv_file)
     for line in reader:
-        lines.append(line)
+        samples.append(line)
+        
+samples = samples[1:]
+random.shuffle(samples)
+train_size = math.floor(0.8 * len(samples))
+train_samples = samples[0:train_size] 
+validation_samples = samples[train_size+1:]
+
+train_generator = generator(train_samples, batch_size=32)
+validation_generator = generator(validation_samples, batch_size=32)
 
 
-images = []
-measurements = []
-
-for line in lines[1:]:
-    # print(line)
-    image_path = './data/'+ line[0]
-    image = cv2.imread(image_path)
-    images.append(image)
-    measurement = float(line[3])
-    measurements.append(measurement)
-    
-    image_flipped = np.fliplr(image)
-    measurement_flipped = -measurement
-    images.append(image_flipped)
-    measurements.append(measurement_flipped)
-    
-
-    
-# print(image_path)
-images = np.array(images)
-measurements = np.array(measurements)
-
-X_train = images
-y_train = measurements
-
-
-
-# data process
+ch, row, col = 3, 160, 320  # Trimmed image format
 
 
 # model
@@ -48,8 +52,10 @@ from keras.layers import Flatten, Dense, Lambda
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
+# nvidia model
 model = Sequential()
-model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
+model.add(Lambda(lambda x: x/127.5 - 1., input_shape=(row, col, ch), output_shape=(row, col, ch)))
+
 model.add(Convolution2D(24,5,5,border_mode='valid', activation='relu', subsample=(2,2)))
 model.add(Convolution2D(36,5,5,border_mode='valid', activation='relu', subsample=(2,2)))
 model.add(Convolution2D(48,5,5,border_mode='valid', activation='relu', subsample=(2,2)))
@@ -61,9 +67,10 @@ model.add(Dense(100, activation='relu'))
 model.add(Dense(50, activation='relu'))
 model.add(Dense(10, activation='relu'))
 model.add(Dense(1, activation='tanh'))
-
 model.compile(loss = 'mse', optimizer = 'adam')
-model.fit(X_train, y_train, validation_split = 0.2, shuffle = True, nb_epoch = 7)
+
+model.fit_generator(train_generator, samples_per_epoch = len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=3)
+
 model.save('model.h5')
 
 
