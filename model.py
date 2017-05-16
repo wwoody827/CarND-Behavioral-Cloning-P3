d20e7ed7cc81e6655832aa9ab18a74dda7d00545
Wudi Wang <wwoody827@gmail.com>
2017-05-16 16:39:06 -0400

    reduce modelsize

--- model.py
+++ model.py
@@ -47,24 +47,28 @@ ch, row, col = 3, 160, 320  # Trimmed image format
 
 # model
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Dropout
+from keras.layers import Flatten, Dense, Lambda, Dropout, Cropping2D
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
 # nvidia model
 model = Sequential()
 model.add(Lambda(lambda x: x/127.5 - 1., input_shape=(row, col, ch), output_shape=(row, col, ch)))
+model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
 
 model.add(Convolution2D(24,5,5,border_mode='valid', activation='relu', subsample=(2,2)))
-model.add(Dropout, 0.2)
+model.add(Dropout(0.2))
 model.add(Convolution2D(36,5,5,border_mode='valid', activation='relu', subsample=(2,2)))
-model.add(Dropout, 0.2)
+model.add(Dropout(0.2))
 model.add(Convolution2D(48,5,5,border_mode='valid', activation='relu', subsample=(2,2)))
-model.add(Dropout, 0.2)
+model.add(Dropout(0.2))
 model.add(Convolution2D(64,3,3,border_mode='valid', activation='relu', subsample=(1,1)))
+model.add(Dropout(0.2))
 model.add(Convolution2D(64,3,3,border_mode='valid', activation='relu', subsample=(1,1)))
+model.add(Dropout(0.2))
+
 model.add(Flatten())
-model.add(Dense(1164, activation='relu'))
+model.add(Dense(400, activation='relu'))
 model.add(Dense(100, activation='relu'))
 model.add(Dense(50, activation='relu'))
 model.add(Dense(10, activation='relu'))
