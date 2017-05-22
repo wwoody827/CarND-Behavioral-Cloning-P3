00bbed6dd8e1ccd1c9a11a0d711fe53eb8ebda9d
Wudi Wang <wwoody827@gmail.com>
2017-05-22 13:45:16 -0400

    add conv

--- model.py
+++ model.py
@@ -77,14 +77,14 @@ from keras import regularizers
 # nvidia model
 model = Sequential()
 model.add(Lambda(lambda x: x/127.5 - 1., input_shape=(row, col, ch), output_shape=(row, col, ch)))
-model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((30,10), (0,0)), input_shape=(160,320,3)))
 
 model.add(Convolution2D(3,1,1,border_mode='valid', activation='relu', subsample=(1,1)))
 
-model.add(Convolution2D(24,5,5,border_mode='valid', activation='relu', subsample=(2,2)))
-model.add(Convolution2D(36,5,5,border_mode='valid', activation='relu', subsample=(2,2)))
+model.add(Convolution2D(32,5,5,border_mode='valid', activation='relu', subsample=(2,2)))
+model.add(Convolution2D(32,5,5,border_mode='valid', activation='relu', subsample=(2,2)))
 model.add(Dropout(0.5))
-model.add(Convolution2D(48,5,5,border_mode='valid', activation='relu', subsample=(2,2)))
+model.add(Convolution2D(64,5,5,border_mode='valid', activation='relu', subsample=(2,2)))
 model.add(Convolution2D(64,3,3,border_mode='valid', activation='relu', subsample=(2,2)))
 model.add(Dropout(0.5))
 model.add(Convolution2D(64,3,3,border_mode='valid', activation='relu', subsample=(1,1)))
@@ -93,8 +93,8 @@ model.add(Dropout(0.2))
 model.add(Flatten())
 model.add(Dense(512, activation='relu', W_regularizer = regularizers.l2(0.001)))
 model.add(Dense(64, activation='relu', W_regularizer = regularizers.l2(0.001)))
-# model.add(Dense(16, activation='relu',  W_regularizer = regularizers.l2(0.001)))
-model.add(Dense(1))
+model.add(Dense(16, activation='relu',  W_regularizer = regularizers.l2(0.001)))
+model.add(Dense(1, activation='tanh'))
 
 model.compile(loss = 'mse', optimizer = 'adam')
 
